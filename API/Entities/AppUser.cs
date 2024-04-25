9807ba9165182bb66a27e9dbd32ff7ead5bc4d84
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2024-04-25 09:56:26 +0100

    api: added relationships and Automappers

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,11 +21,6 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public List<Photo> Photos { get; set; } = new();    
-
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();  
-        }
+        public List<Photo> Photos { get; set; } = new();
     }
 }
