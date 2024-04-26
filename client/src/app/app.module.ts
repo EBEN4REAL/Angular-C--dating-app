fcbff18de155a78ec5543cb609bbb8c958dba6dd
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2024-04-26 06:10:19 +0100

    Client: added loading interceptor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,8 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+
   ],
   bootstrap: [AppComponent]
 })
