5d27ab8b6ec6a220179edf23ac2d93927ac7ec26
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2024-04-26 05:18:04 +0100

    client: added  canDeactivate route guards route guards

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,15 @@
+import { CanDeactivateFn } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { inject } from '@angular/core';
+// import { ConfirmService } from '../_services/confirm.service';
+
+export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  // const confirmService = inject(ConfirmService);
+  
+  if (component.editForm?.dirty) {
+    // return confirmService.confirm();
+    return confirm("Are you sure?")
+  }
+  
+  return true;
+};
